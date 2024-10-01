fee87dbe6115f6433d0a6c8166712035da97b27e
Vivin D <vivin.deena@gmail.com>
2024-10-02 03:45:45 +0530

    telescope changes

--- lua/vivindeena/core/init.lua
+++ lua/vivindeena/core/init.lua
@@ -56,10 +56,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 })
 
- -- vim.g.netrw_browse_split = 0
- -- vim.g.netrw_banner = 0
- -- vim.g.netrw_winsize = 25
-
-vim.api.nvim_create_autocmd("VimEnter", {
-    command = "Neotree toggle"
-})
--- lua/vivindeena/plugins/telescope.lua
+++ lua/vivindeena/plugins/telescope.lua
@@ -1,46 +1,60 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-tree/nvim-web-devicons",
+		},
 
-        tag = "0.1.5",
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
+			telescope.setup({
+				defaults = {
+					path_display = { "smart" },
+				},
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist,
+					},
+				},
+			})
 
-        config = function()
-            require('telescope').setup({})
+			telescope.load_extension("fzf")
 
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>pws', function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end)
-            vim.keymap.set('n', '<leader>pWs', function()
-                local word = vim.fn.expand("<cWORD>")
-                builtin.grep_string({ search = word })
-            end)
-            --[[ vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end) ]]
-            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        end
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            require("telescope").setup {
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-                    }
-                }
-            }
-            require("telescope").load_extension("ui-select")
-        end
-    }
+			-- set keymaps
+			local keymap = vim.keymap -- for conciseness
+
+			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+			keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+			keymap.set(
+				"n",
+				"<leader>fc",
+				"<cmd>Telescope grep_string<cr>",
+				{ desc = "Find string under cursor in cwd" }
+			)
+			keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+			--keymap.set('n', '<leader>vh', builtin.help_tags, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
