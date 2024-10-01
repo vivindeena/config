eabf939e4e374c933b7de98e372076ffce702e15
Vivin D <vivin.deena@gmail.com>
2024-10-02 03:25:39 +0530

    changed some keymaps and added desc for which key

--- lua/vivindeena/plugins/neotree.lua
+++ lua/vivindeena/plugins/neotree.lua
@@ -2,11 +2,15 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
     },
-    config= function()
-        vim.keymap.set("n", "<leader>f", ":Neotree filesystem reveal left<CR>", {})
-    end
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        local keymap = vim.keymap
+        keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+        keymap.set("n", "<leader>er", "<cmd>Neotree focus<CR>", { desc = "Focus and refresh Neo-tree" }) -- refresh file explorer
+    end,
 }
