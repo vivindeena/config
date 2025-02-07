985e87e8ec9f8cdd64195a194bb9d2fbc8eec812
Vivin D <vivin.deena@gmail.com>
2025-02-07 13:51:40 +0530

    feat: rose-pine-ification

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ brew install bat
 #setting up bat
 mkdir -p "$(bat --config-dir)/themes"
 cd "$(bat --config-dir)/themes"
-curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme
+curl --remote-name-all https://raw.githubusercontent.com/rose-pine/tm-theme/main/dist/themes/rose-pineetmTheme
 bat cache --build
 
 # git delta
