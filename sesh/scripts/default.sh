9bac8876f6a4550249d253f86f660f7420693a49
Vivin D <vivin.deena@gmail.com>
2026-07-15 13:49:53 +0530

    chore(config): update shell and CodexBar settings

--- sesh/scripts/default.sh
+++ sesh/scripts/default.sh
@@ -5,10 +5,6 @@ tmux send-keys -t vim "vim" Enter
 # Window 2: shell
 tmux new-window -n zsh
 
-# Window 3: agent
-tmux new-window -n agent
-tmux send-keys -t agent "cc" Enter
-
 # Kill the original first window (usually index 0)
 tmux kill-window -t 1
 
